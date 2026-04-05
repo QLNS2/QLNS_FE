ad4bf354c6c14538e0884051e47ece74784893f8
Trinh Le <trinhcute287@gmail.com>
2026-04-06 01:47:05 +0700

    code giao dien dang nhap & chuc vu

--- src/router/index.js
+++ src/router/index.js
@@ -23,112 +23,97 @@ const routes = [
   },
   {
     path: "/admin/profile",
-    component: () => import("../components/Admin/Profile/index.vue"),
-    beforeEnter: checkLogin,
-  },
-  {
-    path: "/admin/vi-tri-tuyen-dung",
-    component: () => import("../components/Admin/ViTriTuyenDung/index.vue"),
-    beforeEnter: checkLogin,
-  },
-  {
-    path: "/admin/ung-vien",
-    component: () => import("../components/Admin/UngVien/index.vue"),
-    beforeEnter: checkLogin,
-  },
-  {
-    path: "/admin/ho-so-ung-tuyen",
-    component: () => import("../components/Admin/HoSoUngTuyen/index.vue"),
+     // component: () => import("../components/Admin/QuyDinhChoDiem/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/nhan-vien",
-    component: () => import("../components/Admin/NhanVien/index.vue"),
+     // component: () => import("../components/Admin/TieuChiKpi/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/chuc-vu",
-    component: () => import("../components/Admin/ChucVu/index.vue"),
+     component: () => import("../components/Admin/ChiTietHopDong/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/quy-dinh-cho-diem",
-    component: () => import("../components/Admin/QuyDinhChoDiem/index.vue"),
+     // component: () => import("../components/Admin/LoaiHopDong/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/tieu-chi-kpi",
-    component: () => import("../components/Admin/TieuChiKpi/index.vue"),
+     // component: () => import("../components/Admin/ChamCong/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/phong-ban",
-    component: () => import("../components/Admin/PhongBan/index.vue"),
+     // component: () => import("../components/Admin/Luong/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/chi-tiet-hop-dong",
-    component: () => import("../components/Admin/ChiTietHopDong/index.vue"),
+     // component: () => import("../components/Admin/LuongTheoThang/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/loai-hop-dong",
-    component: () => import("../components/Admin/LoaiHopDong/index.vue"),
+     // component: () => import("../components/Admin/KPINhanVien/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/cham-cong",
-    component: () => import("../components/Admin/ChamCong/index.vue"),
+     // component: () => import("../components/Admin/ThuongVaPhat/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/luong",
-    component: () => import("../components/Admin/Luong/index.vue"),
+    // component: () => import("../components/Admin/Luong/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/luong-theo-thang",
-    component: () => import("../components/Admin/LuongTheoThang/index.vue"),
+     // component: () => import("../components/Admin/LuongTheoThang/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/kpi-nhan-vien",
-    component: () => import("../components/Admin/KPINhanVien/index.vue"),
+    // component: () => import("../components/Admin/KPINhanVien/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/thuong-va-phat",
-    component: () => import("../components/Admin/ThuongVaPhat/index.vue"),
+    // component: () => import("../components/Admin/ThuongVaPhat/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/thong-ke-ca-cham-cong",
-    component: () =>
-      import("../components/Admin/ThongKe/ThongKeCaChamCong/index.vue"),
+    // component: () =>
+    //   import("../components/Admin/ThongKe/ThongKeCaChamCong/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/thong-ke-diem-kpi-nhan-vien",
-    component: () =>
-      import("../components/Admin/ThongKe/ThongKeDiemKPINhanVien/index.vue"),
+    // component: () =>
+    //   import("../components/Admin/ThongKe/ThongKeDiemKPINhanVien/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/thong-ke-diem-phat",
-    component: () =>
-      import("../components/Admin/ThongKe/ThongKeDiemPhat/index.vue"),
+    // component: () =>
+    //   import("../components/Admin/ThongKe/ThongKeDiemPhat/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/thong-ke-diem-thuong",
-    component: () =>
-      import("../components/Admin/ThongKe/ThongKeDiemThuong/index.vue"),
+    // component: () =>
+    //   import("../components/Admin/ThongKe/ThongKeDiemThuong/index.vue"),
     beforeEnter: checkLogin,
   },
   {
     path: "/admin/chat",
-    component: () =>
-      import("../components/Admin/Chat/index.vue"),
+    // component: () =>
+    //   import("../components/Admin/Chat/index.vue"), // TODO: tạo file nếu cần
     beforeEnter: checkLogin,
   },
 ];
